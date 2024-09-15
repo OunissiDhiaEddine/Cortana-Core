e90a2cfc52bc35800ca6ccf6c5ae3a69dd840615
OunissiDhiaEddine <dhia.ounissi@outlook.com>
2024-09-15 12:33:24 +0100

    Initial Commit

---
+++ Cortana Core/Cortana_CoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cortana_CoreApp.swift
+//  Cortana Core
+//
+//  Created by Dhia Eddine Ounissi on 2024-09-15.
+//
+
+import SwiftUI
+
+@main
+struct Cortana_CoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
